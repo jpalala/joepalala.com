1e7d749a701218b65397810523e7efa4d5bcd1bb
Joe Palala <jose.palala@live.com>
2022-12-28 16:54:57 +0800

    add devicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,6 @@ export default {
      css: [
        '~/assets/styles/app.styl',
      ],
-  }
+     link: [{ rel:"stylesheet", type:"text/css", href: "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"}]
+  },
 };
