a13df791e120fb5893eafbb3804104ea273c2eee
Joe Palala <jose.palala@live.com>
2022-12-28 18:01:46 +0800

    update appvue

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <style>
   import '~/assets/styles/app.styl';
+  .upstyle { font-size: 20px;}
 </style>
 <template>
   <div>
     <h1>Welcome to joepalala.com</h1>
     <p>Hi I am Joe Palala, a web developer based in the Philippines</p>
-    <div>I work with: <br>  <i class="devicon-vuejs-plain"></i> </div>
+    <div class="upstyle">
+       I work with: <br> <span>React, NodeJS, PHP, Laravel</span>
+    </div>
   </div>
 </template>
