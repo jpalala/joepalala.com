7aea584da3600ae223dffa6292239aabd6c2859d
Joe Palala <jose.palala@live.com>
2022-12-28 17:30:24 +0800

    add vue css

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
+<style>
+ @import url('~
+</style>
 <template>
   <div>
     <h1>Welcome to joepalala.com</h1>
     <p>Hi I am Joe Palala, a web developer based in the Philippines</p>
+    <div>I work with: <br>  <i class="devicon-vuejs-plain"></i> </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default {
   head: {
      css: [
        '~/assets/styles/app.styl',
+       '~/assets/css/devicons.min.css'
      ],
-     link: [{ rel:"stylesheet", type:"text/css", href: "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"}]
   },
 };
